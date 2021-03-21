ebd08802ac482d608bfb7f232cbb9675a1815226
Student <student@Empowrs-MacBook-Pro.local>
2021-03-20 21:48:37 -0400

    tuples challenge and practice

--- 11-tuples/begin/Part 2 - Episode 2 - Tuples - Begin.playground/Contents.swift
+++ 11-tuples/begin/Part 2 - Episode 2 - Tuples - Begin.playground/Contents.swift
@@ -1,1 +1,7 @@
 //: ## Episode 02: Tuples
+let newTuple: (String, Int) = ("Wow", 2)
+print(newTuple.0)
+
+
+let student = (name: "Chase", age: 17, gradeLevel: 11)
+student.name
--- 12-challenge-tuples/begin/Part 2 - Episode 3 - Challenge - Tuples - Begin.playground/Contents.swift
+++ 12-challenge-tuples/begin/Part 2 - Episode 3 - Challenge - Tuples - Begin.playground/Contents.swift
@@ -8,7 +8,7 @@
  */
 
 // TODO: Write solution here
-
+let specialDate: (Int, Int, Int, String) = (09, 14, 2003, "Birthday")
 
 /*:
  Create another tuple, but this time name the constituent components. Give them names related to the data that they contain: month, day, year and description.
@@ -16,15 +16,18 @@
 
 
 // TODO: Write solution here
-
+let date = (month: 09, day: 14, year: 2003, nameOfDay: "Chase Birthday")
 /*:
  In one line, read the day and description values into two constants. You’ll need to use the underscore to ignore the month and year.
  */
 
 // TODO: Write solution here
-
+let (_, day, _, nameOfDay) = date
 
 /*:
  Up until now, you’ve only seen constant tuples. But you can create variable tuples, too. Create one more tuple, like in the exercises above, but this time use var instead of let. Now change the day to a new value. */
 
 // TODO: Write solution here
+var game: (String, String, Int) = ("Apex", "Battle Royale", 2018)
+game.2 = 2019
+print(game.2)
