35e1d9003ca16c25618aefb27a9e7740a4aa3d96
Student <student@Empowrs-MacBook-Pro.local>
2021-03-30 22:02:00 -0400

    while loops challenge and practice

--- 18-while-loops/begin/Part 3 - Episode 2 - While Loops - Begin.playground/Contents.swift
+++ 18-while-loops/begin/Part 3 - Episode 2 - While Loops - Begin.playground/Contents.swift
@@ -1,2 +1,7 @@
 //: ## Episode 02: While Loops
+var i = 1
+while i < 10 {
+    print(i)
+    i += 1
+}
 
--- 19-challenge-while-loops/begin/Part 3 - Episode 3 - Challenge - While Loops - Begin.playground/Contents.swift
+++ 19-challenge-while-loops/begin/Part 3 - Episode 3 - Challenge - While Loops - Begin.playground/Contents.swift
@@ -13,7 +13,12 @@
  
  Explain why the final value of `count` and the value in the last line printed out are different.
  */
-
+var count = 0
+while count < 10 {
+    print("Counting up: \(count)")
+    count += 1
+}
+// it is because the while loop is looping over the block of code until the condtion is false
 
 
 
@@ -30,7 +35,10 @@
  What are three different loop conditions you could use with this loop, and get the same result?
  
  */
-
+repeat {
+    print("Counting up: \(count)")
+    count -= 1
+} while count != 0
 
 
 
@@ -55,8 +63,13 @@
  
  Set the loop condition so that the repeat while loop finishes when the first 6 is rolled.
  */
-
-
+var rollCount = 0
+var roll = 0
+repeat {
+    roll = Int.random(in: 1...6)
+    rollCount += 1
+    print("Roll \(rollCount) gives you a \(roll)")
+} while roll != 6
 
 
 
