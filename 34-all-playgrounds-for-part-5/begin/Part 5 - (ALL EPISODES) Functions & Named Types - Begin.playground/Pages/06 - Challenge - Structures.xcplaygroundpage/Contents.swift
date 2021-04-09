11c27bc4900eee45c1e43c65c5eb6641f3f1e96a
Student <student@Empowrs-MacBook-Pro.local>
2021-04-09 12:01:05 -0400

    structs

--- 34-all-playgrounds-for-part-5/begin/Part 5 - (ALL EPISODES) Functions & Named Types - Begin.playground/Pages/06 - Challenge - Structures.xcplaygroundpage/Contents.swift
+++ 34-all-playgrounds-for-part-5/begin/Part 5 - (ALL EPISODES) Functions & Named Types - Begin.playground/Pages/06 - Challenge - Structures.xcplaygroundpage/Contents.swift
@@ -8,15 +8,32 @@
 2. Create a structure named `Classroom` with two properties: the subject, and an array of students.
 3. Create a method that returns the highest grade in the classroom.
 */
-
-
-/*:
- # Challenge 2
-
- 1. Create an instance of a `Classroom`
- 2. Use the `getHighestGrade` method
-*/
-
+struct Student {
+    let firstName: String
+    let lastName: String
+    let grade: Double
+    
+}
+struct Classroom {
+    let subject: String
+    let students: [Student]
+    
+    func highestGrade() -> Double? {
+        var i: [Double] = []
+        for student in students {
+            i.append(student.grade)
+        }
+        return i.max()
+    }
+    /*:
+     # Challenge 2
+     
+     1. Create an instance of a `Classroom`
+     2. Use the `getHighestGrade` method
+     */
+}
+let aClass: Classroom = Classroom(subject: "Math", students: [Student(firstName: "Chase", lastName: "Palmer", grade: 76.5), Student(firstName: "Bot", lastName: "Bot", grade: 89), Student(firstName: "Capcha", lastName: "bot", grade: 98)])
+aClass.highestGrade()
 
 
 //: [⇒ Next: 07 - Classes](@next)
