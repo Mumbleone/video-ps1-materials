4d05c4a5b314c0eab75251989ffccb974445ccc7
Student <student@Empowrs-MacBook-Pro.local>
2021-04-14 00:01:01 -0400

    class structs work all episodes

--- 34-all-playgrounds-for-part-5/begin/Part 5 - (ALL EPISODES) Functions & Named Types - Begin.playground/Pages/08 - Challenge - Classes.xcplaygroundpage/Contents.swift
+++ 34-all-playgrounds-for-part-5/begin/Part 5 - (ALL EPISODES) Functions & Named Types - Begin.playground/Pages/08 - Challenge - Classes.xcplaygroundpage/Contents.swift
@@ -32,7 +32,28 @@
 //chris.earnExtraCredit()
 //chris.getPassStatus()
 //------------------------------------------------
+class Student {
+    let name: String
+    var grade: Int
+    var pet: String?
+    
+    init (name: String, grade: Int, pet: String? = nil) {
+        self.name = name
+        self.grade = grade
+        self.pet = pet
+    }
+    func getPassStatus(lowestPass: Int = 50) -> Bool {
+        grade >= lowestPass
+    }
+    
+    func earnExtraCredit() {
+        grade += 10
+    }
 
-
+}
+let chris = Student(name: "Chris", grade: 49, pet: "Mango")
+chris.getPassStatus()
+chris.earnExtraCredit()
+chris.getPassStatus()
 
 
